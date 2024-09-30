a3eecb60ce966afc3442f423f213593bfff5b163
zolyomiviktor <72090821+zolyomiviktor@users.noreply.github.com>
2024-09-30 20:09:57 +0200

    Add files via upload

---
+++ beadando.js
@@ -0,0 +1,120 @@
+var valueS = -1;
+parseInt(valueS)
+
+$(document).ready(function() {
+  $("#S").click(function(){ 
+    if (valueS >= 9) {
+      valueS = 0;
+    }  else {
+      valueS++;
+    }
+    document.getElementById("S").innerHTML = valueS;
+  });
+});
+
+var valueE = -1;
+parseInt(valueE)
+
+
+$(document).ready(function() {
+  $("#E").click(function(){ 
+    if (valueE >= 9) {
+      valueE = 0;
+    }  else {
+      valueE++;
+    }
+    document.getElementById("E").innerHTML = valueE;
+    document.getElementById("E2").innerHTML= valueE;
+    document.getElementById("E3").innerHTML= valueE;
+  });
+});
+var valueN = -1;
+parseInt(valueN)
+
+
+$(document).ready(function() {
+  $("#N").click(function(){ 
+    if (valueN >= 9) {
+      valueN = 0;
+    }  else {
+      valueN++;
+    }
+    document.getElementById("N").innerHTML = valueN;
+    document.getElementById("N2").innerHTML= valueN;
+  });
+});
+
+var valueD = -1;
+parseInt(valueD)
+
+$(document).ready(function() {
+  $("#D").click(function(){ 
+    if (valueD >= 9) {
+      valueD = 0;
+    }  else {
+      valueD++;
+    }
+    document.getElementById("D").innerHTML = valueD;
+  });
+});
+
+var valueM= -1;
+parseInt(valueM)
+
+$(document).ready(function() {
+  $("#M").click(function(){ 
+    if (valueM >= 9) {
+      valueM = 0;
+    }  else {
+      valueM++;
+    }
+    document.getElementById("M").innerHTML = valueM;
+    document.getElementById("M2").innerHTML= valueM;
+  });
+});
+
+
+
+var valueO= -1;
+parseInt(valueO)
+
+$(document).ready(function() {
+  $("#O").click(function(){ 
+    if (valueO >= 9) {
+      valueO = 0;
+    }  else {
+      valueO++;
+    }
+    document.getElementById("O").innerHTML = valueO;
+    document.getElementById("O2").innerHTML= valueO;
+  });
+});
+
+
+var valueR = -1;
+parseInt(valueR)
+
+$(document).ready(function() {
+  $("#R").click(function(){ 
+    if (valueR >= 9) {
+      valueR = 0;
+    }  else {
+      valueR++;
+    }
+    document.getElementById("R").innerHTML = valueR;
+  });
+});
+
+var valuey = -1;
+parseInt(valuey)
+
+$(document).ready(function() {
+  $("#y").click(function(){ 
+    if (valuey >= 9) {
+      valuey = 0;
+    }  else {
+      valuey++;
+    }
+    document.getElementById("y").innerHTML = valuey;
+  });
+});
